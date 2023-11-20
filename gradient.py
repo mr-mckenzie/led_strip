e50f8897b0c7ecca937442d2b537f89f6e253418
M <mrgmckenzie@gmail.com>
2023-11-20 11:07:46 +0000

    tweaks to gradient and added new transition

--- gradient.py
+++ gradient.py
@@ -2,6 +2,7 @@ import plasma
 from plasma import plasma_stick
 from random import uniform
 from random import randint
+from random import choice
 import time
 
 number_of_leds = 50
@@ -114,24 +115,39 @@ def fade(led_array = None):
             time.sleep(0.1)
             fading_lumin += lumin_change
         
-def random_animate (led_array):
-    for i in range(0, 51):
-        if i > 0 :
-            led_strip.set_hsv(i-1, led_array[i-1]['hue'], led_array[i-1]['sat'], lumin)
-        if i <= 49:
-            led_strip.set_hsv(i, led_array[i]['hue'], led_array[i]['sat'], 0.1)
-        time.sleep(0.05)
+# def random_animate (led_array):
+#    for i in range(0, 51):
+#        if i > 0 :
+#            led_strip.set_hsv(i-1, led_array[i-1]['hue'], led_array[i-1]['sat'], lumin)
+#        if i <= 49:
+#            led_strip.set_hsv(i, led_array[i]['hue'], led_array[i]['sat'], 0.1)
+#        time.sleep(0.05)
         
 def colour_wheel(led_array):
-    hue_change = 0.3
+    hue_change = 0.75
     while hue_change >= 0:
         for i in range (0,50):
             led_strip.set_hsv(i,led_array[i]['hue']+hue_change,led_array[i]['sat'], lumin)
         time.sleep(0.2)
-        hue_change -= 0.1
+        hue_change -= 0.02
+        
+def group_spiral(led_array):
+    last_led = 50
+    group_size = 5
+    
+    while last_led > 0:
+        for i in range(0,last_led):
+            for j in range (0,group_size):
+                if j <= i:
+                    led_strip.set_hsv(i-j, led_array[last_led-1-j]['hue'], led_array[last_led-1-j]['sat'], lumin)
+            if i >= group_size:
+                led_strip.set_hsv(i-group_size,0,0,0)
+            time.sleep(0.1)
+        last_led -= group_size
+        
         
                 
-animation = [random, downward_rows, upward_rows, downward_spiral, upward_spiral, horizontal, spiral_from_centre, spiral_from_ends, drip_downward, fade, colour_wheel]
+animation = [random, downward_rows, upward_rows, downward_spiral, upward_spiral, horizontal, spiral_from_centre, spiral_from_ends, drip_downward, fade, colour_wheel, group_spiral]
 
 lumin = 0.5
 
@@ -142,8 +158,8 @@ while True:
     time.sleep(2)
 
     starting_hue = uniform(0,1)
-    final_hue = uniform(starting_hue+0.2,starting_hue+0.5)
-    gradient_change = (final_hue - starting_hue)/49
+    final_hue = uniform(starting_hue+0.15,starting_hue+0.70)
+    gradient_change = choice([1, -1]) * (final_hue - starting_hue)/49
 
     starting_sat = uniform(0.4,1)
     final_sat = uniform(0.4,1)
@@ -152,16 +168,13 @@ while True:
     leds = []
     for led in range(0, number_of_leds):
         leds.append({
-            'hue': starting_hue+(led * gradient_change),
+            'hue': starting_hue + 1 + (led * gradient_change),
             'sat': starting_sat + (led * sat_change)})
         
-    animate_on = animation[randint(0,10)]
+    animate_on = animation[randint(0,11)]
     animate_on(leds)
 
     time.sleep(45)
-    
-    #random_animate(leds)
-    #time.sleep(2)
     
     animate_off = animation[randint(0,8)]
     animate_off()
